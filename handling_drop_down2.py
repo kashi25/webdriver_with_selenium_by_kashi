588a6df79df50bb82b52e1ee503aeadac33ce384
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-24 13:01:22 +0545

    update_handling_drop_down_part

--- handling_drop_down2.py
+++ handling_drop_down2.py
@@ -9,5 +9,20 @@ driver = webdriver.Edge()
 driver.get('E:/selinium/env/handeling_drop_down2.html')
 wait = WebDriverWait(driver, 15)
 menu = wait.until(EC.presence_of_element_located((By.NAME, "menu")))
+time.sleep(3)
 
-# breakpoint()
+option = menu.find_element(By.XPATH, "/html/body/form/label[1]/input")
+option.click()
+time.sleep(3)
+
+option = menu.find_element(By.XPATH, "/html/body/form/label[2]/input")
+option.click()
+time.sleep(3)
+
+
+option = menu.find_element(By.XPATH, "/html/body/form/label[3]/input")
+option.click()
+time.sleep(3)
+
+submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+submit.click()
