563da9613bc0f013839464c70a5aba588aefc98c
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-24 09:19:08 +0545

    handling_drop_down2.py_added

---
+++ handling_drop_down2.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Edge()
+
+driver.get('E:/selinium/env/handeling_drop_down2.html')
+wait = WebDriverWait(driver, 15)
+menu = wait.until(EC.presence_of_element_located((By.NAME, "menu")))
+
+# breakpoint()
